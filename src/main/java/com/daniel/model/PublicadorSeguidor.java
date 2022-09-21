3168eea5db474dc92e93327fd0a3117fc646c351
Daniel Bravo <danielbravo1202@gmail.com>
2022-09-20 20:49:17 -0500

    Creación de Tablas y Primeras Pantallas

---
+++ src/main/java/com/daniel/model/PublicadorSeguidor.java
@@ -0,0 +1,60 @@
+package com.daniel.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "publicador_seguidor")
+public class PublicadorSeguidor implements Serializable{
+    
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_publicador")
+    private Persona publicador;
+
+    @ManyToOne
+    @JoinColumn(name = "id_seguidor")
+    private Persona seguidor;
+
+    @Column(name = "fecha",nullable = false)
+    private LocalDateTime fecha;
+
+    public Persona getPublicador() {
+        return publicador;
+    }
+
+    public void setPublicador(Persona publicador) {
+        this.publicador = publicador;
+    }
+
+    public Persona getSeguidor() {
+        return seguidor;
+    }
+
+    public void setSeguidor(Persona seguidor) {
+        this.seguidor = seguidor;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+    
+    
+
+}
---
+++ src/main/java/com/daniel/model/Usuario.java
@@ -0,0 +1,76 @@
+package com.daniel.model;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario implements Serializable {
+    @Id
+    private Integer id;
+
+    //Comportamiento en cascada para reflejar lo que sucede en ambas tablas
+    @OneToOne(cascade = CascadeType.ALL)
+    //Viene a representar la duplicidad de llaves, que es foranea y primaria a la vez
+    @MapsId
+    //Campo para representar la llave foranea
+    @JoinColumn(name = "id",nullable = false)
+    private Persona persona;
+
+    @Column(name = "usuario", nullable = false, length = 30)
+    private String usuario;
+
+    @Column(name = "contrasena", nullable = false, length = 80)
+    private String contrasena;
+
+    @Column(name = "estado", nullable = false, length = 1)
+    private String estado;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
